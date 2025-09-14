129c59cb945e1a6a2832330de0b5881cc38967d1
alekseishmidko <alexeishmidko@gmail.com>
2025-09-15 02:18:53 +0700

    site pages,

---
+++ src/app/(site)/layout.tsx
@@ -0,0 +1,19 @@
+import type { PropsWithChildren } from 'react'
+
+import { Header } from '@/components/layout/header/header'
+import { LayoutContainer } from '@/components/layout/layout-container'
+import { Sidebar } from '@/components/layout/sidebar/sidebar'
+
+export default function SiteLayout({ children }: PropsWithChildren<unknown>) {
+	return (
+		<div className='flex h-full flex-col'>
+			<div className='flex-1'>
+				<div className='fixed inset-y-0 z-50 h-[75px] w-full'>
+					<Header />
+				</div>
+				<Sidebar />
+				<LayoutContainer>{children}</LayoutContainer>
+			</div>
+		</div>
+	)
+}

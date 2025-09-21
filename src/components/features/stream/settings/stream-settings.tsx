f9ba4518ae2cf8ce3a9d9033b52fedc1076251ab
alekseishmidko <alexeishmidko@gmail.com>
2025-09-22 01:32:26 +0700

    features stream, site/:username page

---
+++ src/components/features/stream/settings/stream-settings.tsx
@@ -0,0 +1,51 @@
+import { Pencil } from 'lucide-react'
+import { useTranslations } from 'next-intl'
+
+import { Button } from '@/components/ui/common/button'
+import {
+	Dialog,
+	DialogContent,
+	DialogHeader,
+	DialogTitle,
+	DialogTrigger
+} from '@/components/ui/common/dialog'
+
+import type { FindChannelByUsernameQuery } from '@/graphql/generated/output'
+
+import { useCurrent } from '@/hooks/use-current'
+
+import { ChangeInfoForm } from './change-info-form'
+import { ChangeThumbnailForm } from './change-thumbnail-form'
+
+interface StreamSettingsProps {
+	channel: FindChannelByUsernameQuery['findChannelByUsername']
+}
+
+export function StreamSettings({ channel }: StreamSettingsProps) {
+	const t = useTranslations('stream.settings')
+
+	const { user } = useCurrent()
+
+	const isOwnerChannel = user?.id === channel.id
+
+	if (!isOwnerChannel) {
+		return null
+	}
+
+	return (
+		<Dialog>
+			<DialogTrigger asChild>
+				<Button variant='ghost' size='lgIcon'>
+					<Pencil className='size-5' />
+				</Button>
+			</DialogTrigger>
+			<DialogContent>
+				<DialogHeader>
+					<DialogTitle>{t('heading')}</DialogTitle>
+				</DialogHeader>
+				<ChangeThumbnailForm stream={channel.stream} />
+				<ChangeInfoForm stream={channel.stream} />
+			</DialogContent>
+		</Dialog>
+	)
+}
